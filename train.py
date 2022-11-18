e53670e40dbe860a8bea41d8fd89d84f95e3297e
Moonjong-Shin <mjshin@huinno.com>
2022-11-18 12:50:55 +0900

    test

--- train.py
+++ train.py
@@ -1,26 +1,11 @@
-import dotenv
-import hydra
-from omegaconf import DictConfig
+import mkdocs_gen_files
 
-# load environment variables from `.env` file if it exists
-# recursively searches for `.env` in all folders starting from work dir
-dotenv.load_dotenv(override=True)
+for total in range(19, 100, 20):
+    filename = f"sample/{total}-bottles.md"
 
+    with mkdocs_gen_files.open(filename, "w") as f:
+        for i in reversed(range(1, total + 1)):
+            print(f"{i} bottles of beer on the wall, {i} bottles of beer  ", file=f)
+            print(f"Take one down and pass it around, **{i-1}** bottles of beer on the wall\n", file=f)
 
-@hydra.main(config_path="configs/", config_name="train.yaml")
-def main(config: DictConfig):
-
-    # Imports can be nested inside @hydra.main to optimize tab completion
-    # https://github.com/facebookresearch/hydra/issues/934
-    from src import utils
-    from src.training_pipeline import train
-
-    # Applies optional utilities
-    utils.extras(config)
-
-    # Train model
-    return train(config)
-
-
-if __name__ == "__main__":
-    main()
+    mkdocs_gen_files.set_edit_path(filename, "gen_pages.py")
